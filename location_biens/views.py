43f36986926b8425584b628ede209437ad4eb0b1
226Rach <sabdoulrachid10@gmail.com>
2024-10-15 18:04:53 +0000

    "Updated dashboard.html template and views.py file, modified CSS styles and added navbar and footer elements."

--- location_biens/views.py
+++ location_biens/views.py
@@ -148,7 +148,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
     else:
         form = SignUpForm()
     
